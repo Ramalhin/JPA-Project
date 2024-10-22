3d91f061fb5ed52865185da2ab49cfffe09c651f
João Ramalho Sampaio <104779861+Ramalhin@users.noreply.github.com>
2024-10-22 12:30:38 -0300

    Update Aluno.java

---
+++ src/main/java/com/jpaproject/model/Aluno.java
@@ -0,0 +1,74 @@
+package com.jpaproject.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "alunos")
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String telefone;
+
+    // Construtor padrão
+    public Aluno() {
+    }
+
+    // Construtor com parâmetros
+    public Aluno(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
